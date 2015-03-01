68019aadfca1398270f2cb2573fb83358114e9ef
Eskil <es.3386@es3386-1312mac.local>
2015-03-01 14:45:32 +0100

    Revert b5fc68a..9f3208a
    This rolls back to commit b5fc68ab3d9742617a2c0d083b6b49020a7bf8e5.

---
+++ js/email.js
@@ -0,0 +1,48 @@
+// Email.js version 5
+var tld_ = new Array()
+tld_[0] = "com";
+tld_[1] = "sg";
+tld_[2] = "net";
+tld_[3] = "se";
+tld_[4] = "edu";
+tld_[10] = "ac.nz";
+tld_[11] = "ac.at";
+tld_[12] = "gov.uk";
+tld_[13] = "ac.uk";
+var topDom_ = 13;
+var m_ = "mailto:";
+var a_ = "@";
+var d_ = ".";
+
+function mail(name, dom, tl, params)
+{
+	var s = e(name,dom,tl);
+	document.write('<a href="'+m_+s+params+'">'+s+'</a>');
+}
+function mail2(name, dom, tl, params, display)
+{
+	document.write('<a href="'+m_+e(name,dom,tl)+params+'" class="btn btn-primary btn-xs" role="button" target="_blank">'+display+'</a>');
+}
+function e(name, dom, tl)
+{
+	var s = name+a_;
+	if (tl!=-2)
+	{
+		s+= dom;
+		if (tl>=0)
+			s+= d_+tld_[tl];
+	}
+	else
+		s+= swapper(dom);
+	return s;
+}
+function swapper(d)
+{
+	var s = "";
+	for (var i=0; i<d.length; i+=2)
+		if (i+1==d.length)
+			s+= d.charAt(i)
+		else
+			s+= d.charAt(i+1)+d.charAt(i);
+	return s.replace(/\?/g,'.');
+}
