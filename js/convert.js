232f5aac413e412be0e9272255240f30f3e74e93
Eskil <es.3386@es3386-1312mac.local>
2015-03-05 01:44:48 +0100

    Updated and corrected calculator

--- js/convert.js
+++ js/convert.js
@@ -1,96 +1,84 @@
 function invest() {
     k = $("#k").val();
     b = $("#b").val();
-    sc = Number($("#sc").val());
+    sc = Number($("#sc").val())/100;
     i = Number($("#i").val());
-    mc = marketchange2(i,sc,k,b);
-    $("#s1").val(round(mc.s1));
-    $("#s2").val(round(mc.s2));
-    $("#s").val(round(mc.s));
-    $("#p1").val(round(mc.p1));
-    $("#p2").val(round(mc.p2));
+    mc = marketchange(i,sc,k,b);
+    $("#s1").text(round(mc.s1));
+    $("#s2").text(round(mc.s2));
+    $("#s").text(round(mc.s));
+    $("#p1").text(round(mc.p1));
+    $("#p2").text(round(mc.p2));
     if (round(mc.p1)==round(mc.p2)){
-        $("#p").val(0);
-        $("#avgp").val(0);
+        $("#p").text(0);
+        $("#avgp").text(0);
     } else {
-        $("#p").val(round(mc.p));
-        $("#avgp").val(round(mc.avgp));
+        $("#p").text(round(mc.p));
+        $("#avgp").text(round(mc.avgp));
     };
-    $("#pc").val(round(-i));
+    $("#pc").text(round(-i));
 };
 
-function sell() {
+function sell(origin) {
+    // Calculate initial stuff
     k = $("#k").val();
     b = $("#b").val();
-    sc1 = Number($("#s_sc1").val());
-    sc2 = Number($("#s_sc2").val());
+    sc1 = Number($("#s_sc1").val())/100;
     i = Number($("#s_i").val());
-    ps = Number($("#s_ps").val());
     mc1 = marketchange(i,sc1,k,b);
-    $("#s_sh").val(round(mc1.s)); // Shares held based on initial purchase
-    s1 = scoreToNetSales(sc2,b);
-    $("#s1").val(round(s1));
-    sw = shareWorth(sc2,mc1.s,b,k);
-    $("#s_sw").val(round(sw));
-    $("#s_ps").attr('max', round(sw));
-    mc2 = marketchange(-ps,sc2,k,b);
-    $("#s2").val(round(mc2.s2));
-    $("#s").val(-round(mc2.s));
-    $("#p1").val(round(mc2.p1));
-    $("#p2").val(round(mc2.p2));
-    if (round(mc2.p1)==round(mc2.p2)){
-        $("#p").val(0);
-        $("#avgp").val(0);
-    } else {
-        $("#p").val(round(mc2.p));
-        $("#avgp").val(round(mc2.avgp));
+    $("#s_sh").text(round(mc1.s)); // Shares held based on initial purchase
+    // Don't update current position score if changing certain parameters:
+    if (origin!="s_sc2" && origin!="s_ps" && origin!="b" && origin!="k"){
+        $("#s_sc2").val(mc1.p2);
     }
-    $("#pc").val(round(ps));
+    sc2 = Number($("#s_sc2").val())/100;
+    s1 = scoreToNetSales(sc2,b);
+    $("#s1").text(round(s1));
+    sw = shareWorth(sc2,mc1.s,b,k);
+    $("#s_sw").text(round(sw));
+    $("#s_ps").attr('max', round(sw));
+    if (sw < $("#s_ps").val()){
+        $("#s_ps").val(round(sw));
+    }
+    ps = Number($("#s_ps").val());
+    mc2 = marketchange(-ps,sc2,k,b);
+    $("#s2").text(round(mc2.s2));
+    $("#s").text(-round(mc2.s));
+    $("#p1").text(round(mc2.p1));
+    $("#p2").text(round(mc2.p2));
+    if (round(mc2.p1)==round(mc2.p2)){
+        $("#p").text(0);
+        $("#avgp").text(0);
+    } else {
+        $("#p").text(round(mc2.p));
+        $("#avgp").text(round(mc2.avgp));
+    }
+    $("#pc").text(round(ps));
 }
 
 function round(num) {
-    rounded = Math.round(1000*num)/1000
+    rounded = Math.round(100*num)/100
     return rounded
 }
 
-function marketchange(investment,score,kprm,bprm){
-    p1 = score*kprm;
-    s1 = scoreToNetSales(score,bprm);
-    //pointstozero = netSalesToPoints(
-    s2 = Math.log(Math.exp((2*investment)/(bprm*kprm)+Math.log(1+Math.exp(2*s1/bprm)))-1)*(bprm/2);
-    s = s2-s1;
-    p2 = kprm * Math.exp(2*s2/bprm)/(Math.exp(2*s2/bprm)+1);
-    p = p2-p1;
-    avgp = investment / s;
-    return {s1: s1, s2: s2, s: s, p1: p1, p2: p2, p: p, avgp: avgp}
-}
-
-function marketchange2(investment,yesScore,kprm,bprm){
-    p1 = yesScore*kprm;
+function marketchange(investment,yesScore,kprm,bprm){
+    p1 = yesScore*100;
     s1 = scoreToNetSales(yesScore,bprm);
-    if (investment>=0){
-        // Initial net sales in yes position, correct Yes-score is yesScore
-        s2 = Math.log(Math.exp((2*investment)/(bprm*kprm)+Math.log(1+Math.exp(2*s1/bprm)))-1)*(bprm/2);
-    } else {
-        // Initial net sales in no position, correct noScore is 1-yesScore
-        no_s1 = scoreToNetSales(1-yesScore,bprm);
-        // Net sales in yes position after sale (note minus sign since opposite sales in no position)
-        s2 = -Math.log(Math.exp(-(2*investment)/(bprm*kprm)+Math.log(1+Math.exp(2*no_s1/bprm)))-1)*(bprm/2);
-    }
+    s2 = Math.log(Math.exp((investment)/(bprm*kprm)+Math.log(1+Math.exp(s1/bprm)))-1)*bprm;
     s = s2-s1; // Note that s1, initial net sales, is calculated for the Yes position, and different from the no_s1
-    p2 = kprm * Math.exp(2*s2/bprm)/(Math.exp(2*s2/bprm)+1);
+    p2 = 100 * Math.exp(s2/bprm)/(Math.exp(s2/bprm)+1);
     p = p2-p1;
-    avgp = investment / s;
+    avgp = 100*(investment / s);
     return {s1: s1, s2: s2, s: s, p1: p1, p2: p2, p: p, avgp: avgp}
 }
 
 function scoreToNetSales(score,b){
-    netSales = 0.5 * b * Math.log(score/(1-score));
+    netSales = b * Math.log(score/(1-score));
     return netSales;
 }
 
 function netSalesToPoints(s1,s2,b,k){
-    points = 0.5 * b * k * (Math.log(1+Math.exp(2*s2/b))-Math.log(1+Math.exp(2*s1/b)));
+    points = b * k * (Math.log(1+Math.exp(s2/b))-Math.log(1+Math.exp(s1/b)));
     return points;
 }
 
@@ -139,20 +127,20 @@ $(document).ready(function () {
             if (selectedType=="invest"){
                 invest();
             } else if (selectedType=="sell"){
-                sell();
+                sell(this.id);
             }
         } else if ($(this).val()<Number(this.min)  && this.hasAttribute("min")){
             $(this).val(Number(this.min));
             if (selectedType=="invest"){
                 invest();
             } else if (selectedType=="sell"){
-                sell();
+                sell(this.id);
             }
         } else {
             if (selectedType=="invest"){
                 invest();
             } else if (selectedType=="sell"){
-                sell();
+                sell(this.id);
             }
         };
     }); 
@@ -163,7 +151,7 @@ $(document).ready(function () {
         if (selectedType=="invest"){
             invest();
         } else if (selectedType=="sell"){
-            sell();
+            sell(this.id);
         }
     }); 
 });
